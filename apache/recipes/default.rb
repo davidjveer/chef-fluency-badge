2eeec128def5694b3d0456bcd4f0e0f9aa4e0711
David J. Veer <david.veer@me.com>
2018-06-18 20:53:25 +1200

    Changed for platform

--- apache/recipes/default.rb
+++ apache/recipes/default.rb
@@ -4,8 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-  package_name 'httpd'
+  package_name package
   action :install
 end
 
