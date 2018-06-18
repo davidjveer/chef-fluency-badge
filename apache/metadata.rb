324a634137bd91fc9d341cf9a8eec890d1932d2e
David J. Veer <david.veer@me.com>
2018-06-18 20:55:47 +1200

    Bumped version

--- apache/metadata.rb
+++ apache/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.1.1'
+version '0.1.3'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
